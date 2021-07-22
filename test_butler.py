08e4455fc829e3d71ec7d4697ae0abec2c9088c0
Kylem <kylem.workemail@gmail.com>
2021-07-22 00:00:12 -0700

    add Travis Ci integration

--- test_butler.py
+++ test_butler.py
@@ -1,13 +1,14 @@
 import Butler
+import pytest
 
 
-def test_get(self):
+def test_get():
     butler = Butler.Butler("Jarvis")
     assert butler.get_name() == "Jarvis"
 
 
-def test_set(self):
+def test_set():
     butler = Butler.Butler("Jarvis")
     butler.set_name("Jenkins")
-    assert self.butler.get_name() == "Jenkins"
+    assert butler.get_name() == "Jenkins"
 
