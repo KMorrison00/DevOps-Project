c955b9e7a7a7cf35ad87b25dbc956e621ab69f71
Kylem <kylem.workemail@gmail.com>
2021-07-21 21:07:22 -0700

    add Travis Ci integration

--- ButlerTests.py
+++ ButlerTests.py
@@ -1,16 +1,18 @@
 import unittest
 import Butler
 
+
 class MyTestCase(unittest.TestCase):
 
     butler = Butler.Butler()
 
     def test_get(self):
-        self.assertEqual(self.butler.get_name(), "Jarvis")
+        assert self.butler.get_name() == "Jarvis"
 
     def test_set(self):
         self.butler.set_name("Jenkins")
-        self.assertEqual(self.butler.get_name(), "Jenkins")
+        assert self.butler.get_name() == "Jenkins"
+
 
 if __name__ == '__main__':
     unittest.main()
